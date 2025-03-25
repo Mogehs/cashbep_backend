979ff9d05750d31acfdecae602db6c378cfb6736
Mogehs <mugheesurrehman06@gmail.com>
2025-03-25 18:19:07 +0500

    added the frontend

--- server.js
+++ server.js
@@ -17,17 +17,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-
 app.use(
   cors({
-    origin: process.env.BASE_URL,
+    origin: "https://bmx-atventure.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Add the required methods
   })
 );
 
-app.get('/',(req,res)=>{
-  res.send("Backend is running.......")
-})
+app.get("/", (req, res) => {
+  res.send("Backend is running.......");
+});
 
 app.use(cookieParser());
 
