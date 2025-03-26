d924c834681dfbde83abdc77d09aa7308d65a672
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-03-26 16:14:19 +0500

    Updated commit message without setting cookies

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    // origin: "https://bmx-atventure.vercel.app",
-    origin: process.env.FRONT_END_URL,
+    origin: "https://bmx-atventure.vercel.app",
+    // origin: process.env.FRONT_END_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
